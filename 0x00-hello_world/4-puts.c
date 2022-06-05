a853f423bc887a72152e8852216959b4449b08ce
Gedeon-Girma <gedeon.girma@aait.edu.et>
2022-06-05 15:03:43 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,4 @@
-#include <stdio.h>
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+#!/bin/bash
 int main(void)
-{
-	       puts('\"Programming is like building a multilingual puzzle");
-	       return (0);
-}	       
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
